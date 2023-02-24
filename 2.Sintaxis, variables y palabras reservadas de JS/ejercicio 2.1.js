54fde02dfab6ad4683e5ffeee9d2a7335904e2fa
maxtrueno <maxtrueno_14@hotmail.com>
2023-02-24 15:58:22 +0100

    subida

---
+++ 2.Sintaxis, variables y palabras reservadas de JS/ejercicio 2.1.js
@@ -0,0 +1,11 @@
+const libro_favorito ={
+    titulo: "berserk",
+    autor: "Kentaro Miura",
+    año_publicacion: 1989,
+    url: "https://es.wikipedia.org/wiki/Berserk_(manga)"};
+
+const fecha_nacimiento = new Date(1997, 6, 22);
+
+const lista = ["Miguel Ángel", 25, 0, fecha_nacimiento, libro_favorito];
+
+console.log(lista);
