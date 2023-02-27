ab3a20176c579dffdb2a5938bda4bd53cd479546
maxtrueno <maxtrueno_14@hotmail.com>
2023-02-27 10:54:37 +0100

    commit

---
+++ 5.Números en JavaScript/ejercicio 5.1.js
@@ -0,0 +1,14 @@
+//Crea un archivo JS que contenga las siguientes líneas
+
+// Una variable que contenga tu altura en centímetros (entero)
+var altura_cm = 185;
+// Una variable que contenga tu altura en metros (número de coma flotante)
+var altura = 1.85;
+// Una variable que contenga tu peso en kilogramos (número de coma flotante)
+var peso = 68.5;
+// Una variable que contenga tu altura en metros redondeada hacia arriba
+var redondeo = Math.round(altura);
+// Una variable que contenga tu peso en kilogramos redondeado hacia abajo
+var redondeoo = Math.trunc(peso);
+// Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1" es igual al máximo valor que se puede obtener en Javascript
+var booleana = (Infinity == Infinity +1)
